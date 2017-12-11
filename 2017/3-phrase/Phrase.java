1510b56c9af4836a073a6c19a2dca58f424f7503
Thofe <bmitrick@gmail.com>
2017-12-11 10:24:28 -0500

    Add files via upload

--- 2017/3-phrase/Phrase.java
+++ 2017/3-phrase/Phrase.java
@@ -26,7 +26,20 @@ public class Phrase
      * @return -1 if the nth occurence does not exist, otherwise returns the nth occurence
      */
     public int findNthOccurrence(String str, int n){
-        return currentPhrase.indexOf(str, n);
+        int index = currentPhrase.indexOf(str);
+        
+        if (index == -1){
+            return -1;
+        }
+        
+        for (int i=1; i<n; i++){
+            index = currentPhrase.indexOf(str, index+1);
+            if (index == -1){
+                return -1;
+            }
+        }
+        
+        return index;
     }
     
     /**
@@ -36,7 +49,7 @@ public class Phrase
      * @param n the nth occurence to be replaced
      * @param repl the segment to replace str with
      */
-    public void replaceNthOccurence(String str, int n, String repl){
+    public void replaceNthOccurrence(String str, int n, String repl){
         int index = findNthOccurrence(str, n);
         
         if (index == -1){
@@ -50,6 +63,33 @@ public class Phrase
     }
     
     /**
+     * Returns the index of the last occurence of str in the current phrase
      * 
+     * @param str the string that is looked for
+     * @return the index of the last occurence, -1 will be returned if str is not present
      */
+    public int findLastOccurrence(String str){
+        int count = 0;
+        Phrase temp = new Phrase(currentPhrase);
+        
+        while (temp.findNthOccurrence(str, 1) != -1){
+            count++;
+            temp.replaceNthOccurrence(str,1, "");
+        }
+        
+        if (count == 0){
+            return -1;
+        }else{
+            return findNthOccurrence(str, count);
+        }
+    }
+    
+    /**
+     * Returns a string of the currentPhrase
+     * 
+     * @return a string of the currentPhrase
+     */
+    public String makeString(){
+        return currentPhrase.toString();
+    }
 }
