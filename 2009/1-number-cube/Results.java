969a0f88984d4b453367cc66e12380de4e1e8fce
Thofe <bmitrick@gmail.com>
2018-02-14 10:27:18 -0500

    Add files via upload

---
+++ 2009/1-number-cube/Results.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Results here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Results
+{
+    private NumberCube cube;
+    
+    /**
+     * constructs the Results class based around a single given cube
+     * 
+     * @param cube a cube to be used
+     */
+    public Results(NumberCube cube){
+        this.cube = cube;
+    }
+    
+    /**
+     * 
+     */
+}
